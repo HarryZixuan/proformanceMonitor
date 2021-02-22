39de630803758fe69d36b34ac74200c0b81c512d
HarryZixuan <zixuanzhang@cmail.carleton.ca>
2021-02-21 19:30:20 -0500

    setup timerTask in CPUFragment, for test purpose

--- app/src/main/java/com/example/proformancemonitor/CPUFragment.java
+++ app/src/main/java/com/example/proformancemonitor/CPUFragment.java
@@ -1,7 +1,10 @@
 package com.example.proformancemonitor;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +14,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class CPUFragment extends Fragment {
-
-    View view;
-
     Bundle bundle;
     String ipAddress;
     TextView tv_cpuUsage;
+    int test = 0;
+    private Timer timer;
+    private TimerTask task;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,27 +39,45 @@ public class CPUFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_cpu,container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tv_cpuUsage = getView().findViewById(R.id.tv_cpuUsage);
+
+        final Handler handler = new Handler();
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        updateCpuInfo mAsync = new updateCpuInfo();
+                        mAsync.execute();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 1000); //Every 1 second
+
+
+    }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+    protected class updateCpuInfo extends AsyncTask <String, Void, String> {
 
-        view = getView();
-
-        tv_cpuUsage = view.findViewById(R.id.tv_cpuUsage);
-        int test = 0;
-
-        while(isAdded()){
+        @Override
+        protected String doInBackground(String... strings) {
             test ++;
+            return null;
+        }
 
-            tv_cpuUsage.setText(test);
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+            tv_cpuUsage.setText(String.valueOf(test));
         }
     }
 }
